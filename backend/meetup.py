bcd55ef959aa64907f31a4125ca261aff092fade
Farhia M <farhia.fcb@gmail.com>
2018-04-22 15:59:21 +0200

    cleaned the code and added function that can be called from the outside

--- backend/meetup.py
+++ backend/meetup.py
@@ -1,24 +1,28 @@
 import requests
 from meetup_key import my_key
 
-text = input("What do you want to find? ")
-radius = "50"
-key = my_key             
+def get_events( text ):
+    key = my_key             
+    radius = "50"
+    data = requests.get("https://api.meetup.com/find/upcoming_events?&key={}&sign=true&photo-host=public&page=20&text={}&radius={}".format(key, text, radius))
 
-data = requests.get("https://api.meetup.com/find/upcoming_events?&key={}&sign=true&photo-host=public&page=20&text={}&radius={}".format(key, text, radius))
+    events = data.json()
+    eventsFound = False
+    return events
 
-events = data.json()
-eventsFound = False
+if __name__ == '__main__':
+    text = input("What do you want to find? ")
+    events = get_events(text,radius)
 
-for event in events["events"]:
-    try:
-        print(event["name"])
-        print(event["description"])
-        print(event["venue"]["city"])
-        eventsFound = True
-        print()
-    except:
-        continue
+    for event in events["events"]:
+        try:
+            print(event["name"])
+            #print(event["description"])    CURRENT DISABLED DUE TO ALOT OF TEXT OUTPUT
+            print(event["venue"]["city"])
+            eventsFound = True
+            print()
+        except:
+            continue
 
-if not eventsFound:
-    print("No matching events found")
+    if not eventsFound:
+        print("No matching events found")
