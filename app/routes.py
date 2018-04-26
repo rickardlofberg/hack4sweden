6855256f6d46dbe28f36698c214884197e35a3ab
mardub1635 <pyou.pyou@laposte.net>
2018-04-26 17:51:49 +0200

    a basic input form for the app in index.html - imports backend in routes.py - interface is called from app/route.py - "This is just a test page" is now an input form - user can input a skill on the index.html page - index.html post form returns taxonomy match and a prognosis

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,24 @@
-from flask import render_template
+# -*- coding: utf-8 -*-
+from flask import render_template,request
 from app import app
-    
+import sys
+#get the path toward the backend folder
+sys.path.insert(0, '..')
+from backend.interface import SearchInterface
+
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/')
+@app.route('/', methods=['POST'])
+def form_post():
+    """Inputs the user request, returns results from interface"""
+    text = request.form['text']
+    s = SearchInterface()
+    s.search_job(text)
+    job=s.latest_query
+    return u"You searched for: {}".format(s.latest_query)+u"<br/>Best match in taxonomy is: {}".format(s.taxonomy_job)+"<br/>The current prognosis for job is: {}".format(s.current_year_forecast)
+if __name__ == '__main__':
+    app.run()
--- todo.md
+++ todo.md
@@ -3,7 +3,7 @@
 - Update examples **(DONE)**
 - Integrate ontology to interface **(DONE)**
 - Integrate meetup api to interface **(DONE)**
-- Add Flask application for local testing **(done?)**
-- Break out "convert requested data to json check" to own function/method? **(done?)**
+- Add Flask application for local testing **(ongoing)**
+- Break out "convert requested data to json check" to own function/method? **(DONE)**
 - Better solution to go from listed jobs to taxonomy jobs.
 - How can we find the best match in taxonomy when going from the ontology match?
